8507455c0af653195695593999fb8b7447d8b35d
minhthi152 <minhthi152@gmail.com>
2022-04-01 16:57:16 +0700

    updated

--- Nhap/src/Array.java
+++ Nhap/src/Array.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-        double[] myArr = {1,2,3,4,5};
-        String newArr = x2array(myArr);
-//        x2array(myArr);
-        System.out.println(newArr);
+        static public void f() {};
+
+        static void f() {};
+
+        public void static f() {};
+
+        public static void f() {};
+
     }
-    public static String x2array(double[] array){
-        int i;
-        for(i = 0; i < array.length; i++){
-            array[i] = array[i]*2;
-        }
-        return Arrays.toString(array);
-    }
+
+
 
 }
 
