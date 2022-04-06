39eac5345ab369b07353011e2eeeff65de003de2
minhthi152 <minhthi152@gmail.com>
2022-04-06 10:48:17 +0700

    add

---
+++ accessModifier-staticMethod-staticProperty/Student/src/Student.java
@@ -0,0 +1,31 @@
+public class Student {
+    private String name = "John";
+    private String classes = "C02";
+    Student(){
+
+    }
+    public void setName(String name){
+        this.name = name;
+
+    }
+
+    public void setClasses(String classes){
+        this.classes = classes;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    @Override
+    public String toString() {
+        return "Student " +
+                 name +
+                ", classes = " + classes;
+    }
+}
---
+++ accessModifier-staticMethod-staticProperty/Student/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Thi");
+        s1.setClasses("C0222H1");
+        System.out.println(s1.toString());
+    }
+}
